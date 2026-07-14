50976813a8fd9e73ff1d5f9764b67cfabce53eb2
Edwin Rivera <gaedwin92@gmail.com>
2026-07-14 15:15:07 -0600

    fix: config page updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  // lo cambiás cuando tengas el dominio real del cliente
+  site: 'https://github.com/roedwin-afk',
+  base: '/clinica-dental-landing/',
+
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
